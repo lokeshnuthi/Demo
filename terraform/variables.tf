05eb3217b180d9bb8444cc71f1cc93363ecf86bf
lokesh143nuthi <64340215+lokesh143nuthi@users.noreply.github.com>
2026-02-25 19:29:41 -0600

    updated terraform file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "ami_id" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-}
+# variable "ami_id" {
+#   description = "AMI ID for EC2 instance"
+#   type        = string
+# }
